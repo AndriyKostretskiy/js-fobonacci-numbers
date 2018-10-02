d7da4fd771354dc36240065e413e579fa8221f6a
Andriy <andriy.kostretskiy@gmail.com>
2018-10-02 15:49:58 +0000

    Update

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const numb = parseFloat(prompt('Enter the number of Fibonacci'));
+alert("Loop method: " + fiboInLoop(numb));
+alert("Array method: " + fiboInArray(numb));
+alert("Recurcive method: " + fiboRecurcive(numb));
+
+function fiboInLoop(numb){
+    let fiboNumb_1 = 1, fiboNumb_2 = 1;
+    for(let i = 3; i <= numb; i++){
+        let fiboNextNumb = fiboNumb_1 + fiboNumb_2;
+        fiboNumb_1 = fiboNumb_2;
+        fiboNumb_2 = fiboNextNumb;
+    }
+    return fiboNumb_2;
+}
+
+function fiboInArray(numb){
+    let fiboArr = [1, 1];
+    for(let i = 2; i < numb; i++){
+        fiboArr.push(fiboArr[i-1] + fiboArr[i-2])
+    }
+    return fiboArr.pop();
+}
+
+function fiboRecurcive(numb){
+    if(numb <= 1) { 
+       return numb;
+    }
+    return fiboInLoop(numb - 1) + fiboInLoop(numb - 2);
+}
+    
